2a02cbeff8db6e366f69c317e86402d232e5b659
JOnata <jOnatafrancisco@gmail.com>
2021-11-07 14:43:17 -0300

    testando criação tabela H2

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -2,10 +2,20 @@ package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-//atribustos
+	
+	@Id
+	//estratégia de geração automatica dos id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//atributos
 	private Integer id;
 	private String nome;
 //construtor vazio - instacia objeto sem jogar nenhuma infor p os atributos
