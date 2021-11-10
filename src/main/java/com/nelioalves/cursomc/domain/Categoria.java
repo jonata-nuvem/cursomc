3e1914a1f019acdde7c9d55c9e2510ef6b1e9a68
JOnata <jOnatafrancisco@gmail.com>
2021-11-10 13:39:01 -0300

    Banco conectado e funcionando H2. Obs: JDK 17 para JDK 11. Usei---jdbc:h2:mem:teste

--- src/main/java/com/nelioalves/cursomc/domain/Categoria.java
+++ src/main/java/com/nelioalves/cursomc/domain/Categoria.java
@@ -6,10 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+//import javax.persistence.Table;
 
 @Entity
-@Table(name="categoria")
+//@Table(name="tb_category")
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
